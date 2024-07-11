0ba247c25510875ded77907d523bc4012553f110
Mykhailo <Mykhailo.Gushko@ruhr-uni-bochum.de>
2024-07-11 12:22:34 +0200

    ok

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <sqlite3.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -59,17 +60,29 @@ int roomComparer(const void *r1, const void *r2) {
   return (pr1->priority - pr2->priority);
 }
 
-Room *roomRandomizer(int firstRoom, int lastRoom, int *not_occupied,
-                     int *weeks_not_on_duty) {
-  int s = sizeof(not_occupied) / sizeof(not_occupied[0]);
-  qsort(not_occupied, s, sizeof(int), compare);
+Room *roomRandomizer(sqlite3 *db, Room rooms[], size_t rooms_size,
+                     int firstRoom, int lastRoom, int *not_occupied,
+                     int not_occupied_quantity, int *weeks_not_on_duty) {
+
+  // Generating who was not on the duty for how long from the existing sql data.
+  char *sql = "SELECT week_start FROM duty_week WHERE condition";
+
+  sqlite3_stmt *stmt;
+  int prep = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+
+  char *week_beginning_array[firstRoom - lastRoom - not_occupied_quantity];
+  while ((prep = sqlite3_step(stmt)) == SQLITE_ROW) {
+    char *week_begining = sqlite3_column_text(stmt, 0);
+  }
+
+  // Starting to
+  qsort(not_occupied, not_occupied_quantity, sizeof(int), compare);
   int numberOfRooms = lastRoom - firstRoom - sizeof(not_occupied);
-  Room rooms[numberOfRooms];
   int r = firstRoom;
   int not_occupied_iterator = 0;
-  while (r < sizeof(rooms)) {
+  while (r < rooms_size) {
     int numberOfPeople =
-        lastRoom - firstRoom - sizeof(not_occupied) / sizeof(not_occupied[0]);
+        lastRoom - firstRoom - not_occupied_quantity / sizeof(not_occupied[0]);
     int currentRoom = firstRoom + r;
     if (not_occupied[not_occupied_iterator] != currentRoom) {
       int rooms_iter = 0;
@@ -86,13 +99,12 @@ Room *roomRandomizer(int firstRoom, int lastRoom, int *not_occupied,
     }
     r++;
   }
-  qsort(rooms, sizeof(rooms) / sizeof(Room), sizeof(Room), roomComparer);
-  return &rooms;
+  qsort(rooms, rooms_size / sizeof(Room), sizeof(Room), roomComparer);
+  return rooms;
 }
 
-int populateDBWithRooms(sqlite3 *db, Room *rooms, int day, int month,
-                        int year) {
-  int r = 0;
+int populateDBWithRooms(sqlite3 *db, Room rooms[], size_t rsize, int day,
+                        int month, int year) {
   for (int i = 0; i < 10; i++) {
     time_t rawtime;
     char formatted_time[80];
@@ -105,25 +117,31 @@ int populateDBWithRooms(sqlite3 *db, Room *rooms, int day, int month,
     sqlite3_stmt *stmt;
     int rc = sqlite3_prepare_v2(db, insertionQuery, -1, &stmt, NULL);
     if (rc != SQLITE_OK) {
-      // If statement preparation fails, print an error message, close the
-      // database, and exit
       fprintf(stderr, "Error preparing statement: %s\n", sqlite3_errmsg(db));
       sqlite3_close(db);
       return 1;
     }
     sqlite3_bind_text(stmt, 1, formatted_time, -1, SQLITE_TRANSIENT);
-    sqlite3_bind_int(stmt, 1, rooms[r].room_number);
-    rooms[r].priority = (sizeof(rooms) / sizeof(rooms[0]) / 2) +
-                        ((float)(rand() % 400) / 100 - 2);
-    qsort(rooms, sizeof(rooms) / sizeof(Room), sizeof(Room), roomComparer);
+
+    sqlite3_bind_int(stmt, 2, rooms[0].room_number);
+    rooms[0].priority =
+        (rsize / sizeof(rooms[0]) / 2) + ((float)(rand() % 400) / 100 - 2);
+    qsort(rooms, rsize / sizeof(Room), sizeof(Room), roomComparer);
+
+    sqlite3_bind_int(stmt, 3, rooms[0].room_number);
+    rooms[0].priority =
+        (rsize / sizeof(rooms[0]) / 2) + ((float)(rand() % 400) / 100 - 2);
+    qsort(rooms, rsize / sizeof(Room), sizeof(Room), roomComparer);
+
     int exe = sqlite3_step(stmt);
-    if (rc != SQLITE_DONE) {
+    if (exe != SQLITE_DONE) {
       fprintf(stderr, "Error inserting data: %s\n", sqlite3_errmsg(db));
       sqlite3_close(db);
       return 1;
     }
     sqlite3_finalize(stmt);
   }
+
   fprintf(stdout, "Data inserted successfully.\n");
   return 0;
 }
